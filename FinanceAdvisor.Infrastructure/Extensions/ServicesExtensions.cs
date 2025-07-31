f553504894edd0f82d331690268fe84df212aa6c
Yavor79 <yavor_04@abv.bg>
2025-07-31 22:52:26 +0300

    added repositories and services seeding extension methods

---
+++ FinanceAdvisor.Infrastructure/Extensions/ServicesExtensions.cs
@@ -0,0 +1,69 @@
+﻿using FinanceAdvisor.Domain.IRepositories;
+using FinanceAdvisor.Infrastructure.Identity;
+using FinanceAdvisor.Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceAdvisor.Infrastructure.Extensions
+{
+    public static class ServicesExtensions
+    {
+        private static readonly string ProjectInterfacePrefix = "I";
+        private static readonly string ServiceTypeSuffix = "Service";
+
+        private static readonly string RepositoryTypeSuffix = "Repository";
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services, Assembly modelsAssembly)
+        {
+            Type[] repoClasses = modelsAssembly.GetTypes()
+                .Where(t => t.Name.EndsWith(RepositoryTypeSuffix)
+                && !t.IsInterface 
+                && !t.IsAbstract)
+                .ToArray();
+
+            foreach (Type repoClass in repoClasses)
+            {
+                Type? repoInterface = repoClass
+                    .GetInterfaces()
+                    .FirstOrDefault(i => i.Name == $"{ProjectInterfacePrefix}{repoClass.Name}");
+
+                if (repoInterface == null)
+                {
+                    throw new ArgumentException(string.Format($"InterfaceNotFound for {repoClass.Name}"));
+                }
+
+                services.AddScoped(repoInterface, repoClass);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterServices(this IServiceCollection services, Assembly assembly)
+        {
+            Type[] serviceClasses = assembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith(ServiceTypeSuffix) 
+                    && !t.IsInterface)
+                .ToArray();
+
+            foreach (Type serviceClass in serviceClasses)
+            {
+                Type? serviceInterface = serviceClass
+                    .GetInterfaces()
+                    .FirstOrDefault(i => i.Name == $"{ProjectInterfacePrefix}{serviceClass.Name}");
+
+                if (serviceInterface == null)
+                {
+                    throw new ArgumentException($"InterfaceNotFound for {serviceClass.Name}");
+                }
+
+                services.AddScoped(serviceInterface, serviceClass);
+            }
+            return services;
+        }
+    }
+}
--- FinanceAdvisor/Program.cs
+++ FinanceAdvisor/Program.cs
@@ -3,27 +3,61 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FinanceAdvisor.Infrastructure;
+using FinanceAdvisor.Infrastructure.Repository;
+//using FinanceAdvisor.Infrastructure.Seed;
+//using static FinanceAdvisor.Infrastructure.Seed.DbSeeder;
+using FinanceAdvisor.Infrastructure.Identity;
+using FinanceAdvisor.Application.Interfaces;
+using FinanceAdvisor.Application.Services;
+using FinanceAdvisor.Application.IRepos;
+using FinanceAdvisor.Infrastructure.Extensions;
+using FinanceAdvisor.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore.Internal;
+
 
 namespace FinanceAdvisor
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //builder.WebHost.ConfigureKestrel(options =>
+            //{
+            //    options.ListenLocalhost(5001, o => o.UseHttps());
+            //    options.ListenLocalhost(5000); // HTTP
+            //});
+
+
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<FinanceDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+          
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
             {
-                options.SignIn.RequireConfirmedAccount = false;
+                ConfigureIdentity(builder, options);
             })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+            .AddEntityFrameworkStores<FinanceDbContext>()
+            .AddRoles<IdentityRole<Guid>>()
+            .AddSignInManager<SignInManager<ApplicationUser>>()
+            .AddUserManager<UserManager<ApplicationUser>>();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowMVC", policy =>
+                {
+                    policy.WithOrigins("https://localhost:7053;http://localhost:5024") 
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            builder.Services.RegisterRepositories(typeof(CreditConsultationCycleRepository).Assembly);
+            builder.Services.RegisterServices(typeof(CreditConsultationCycleService).Assembly);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,6 +65,31 @@ namespace FinanceAdvisor
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            //AutoMapperConfig.Initialize();
+
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+
+            //    var appUserSeeder = new ApplicationUserSeeder();
+            //    await appUserSeeder.SeedAsync(services);
+
+            //    var advisorSeeder = new AdvisorSeeder();
+            //    await advisorSeeder.SeedAsync(services);
+
+            //    var consultationSeeder = new CreditConsultationCycleSeeder();
+            //    await consultationSeeder.SeedAsync(services);
+            //}/
+            
+
+            app.UseCors("AllowMVC");
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    await DbSeeder.SeedDataAsync(services);
+            //}
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -43,10 +102,36 @@ namespace FinanceAdvisor
 
             app.UseAuthorization();
 
-
+            
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void ConfigureIdentity(WebApplicationBuilder webAppBuilder, IdentityOptions identityOptions)
+        {
+            identityOptions.Password.RequireDigit =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:Password:RequireDigits");
+            identityOptions.Password.RequireLowercase =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+            identityOptions.Password.RequireUppercase =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+            identityOptions.Password.RequireNonAlphanumeric =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumerical");
+            identityOptions.Password.RequiredLength =
+                webAppBuilder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
+            identityOptions.Password.RequiredUniqueChars =
+                webAppBuilder.Configuration.GetValue<int>("Identity:Password:RequiredUniqueCharacters");
+
+            identityOptions.SignIn.RequireConfirmedAccount =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+            identityOptions.SignIn.RequireConfirmedEmail =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedEmail");
+            identityOptions.SignIn.RequireConfirmedPhoneNumber =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedPhoneNumber");
+
+            identityOptions.User.RequireUniqueEmail =
+                webAppBuilder.Configuration.GetValue<bool>("Identity:User:RequireUniqueEmail");
         }
     }
 }
